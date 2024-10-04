2337c59885e3cbd820777721ec17bcaf66944359
Jan Horacek <me@apophis.cz>
2024-10-04 21:11:20 +0200

    Add standard czech translation file.

---
+++ mtb-config_cz.ts
@@ -0,0 +1,141 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/mainwindow.ui" line="23"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="126"/>
+        <source>MTB Configuration Tool</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="73"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="146"/>
+        <source>Address DEC</source>
+        <translation>Adresa DEC</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="81"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="145"/>
+        <source>Address HEX</source>
+        <translation>Adresa HEX</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="86"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="144"/>
+        <source>Address BIN</source>
+        <translation>Adresa BIN</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="91"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="143"/>
+        <source>Name</source>
+        <translation>Jméno</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="96"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="142"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="101"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="141"/>
+        <source>State</source>
+        <translation>Stav</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="106"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="140"/>
+        <source>Firmware</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="111"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="139"/>
+        <source>Bootloader</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="116"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="138"/>
+        <source>Error</source>
+        <translation>Porucha</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="121"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="137"/>
+        <source>Warning</source>
+        <translation>Varování</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="126"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="136"/>
+        <source>Beacon</source>
+        <translation>Maják</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="144"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="147"/>
+        <source>MTB Daemon</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="153"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="148"/>
+        <source>MTB-USB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="159"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="149"/>
+        <source>Help</source>
+        <translation>Nápověda</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="170"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="127"/>
+        <source>Connect</source>
+        <translation>Připojit</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="178"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="128"/>
+        <source>Disconnect</source>
+        <translation>Odpojit</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="183"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="129"/>
+        <source>Connection Settings</source>
+        <translation>Nastavení připojení</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="191"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="130"/>
+        <source>MTBbus Settings</source>
+        <translation>Nastavení MTBbus</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="196"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="131"/>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.ui" line="199"/>
+        <location filename="build/mtb-config_autogen/include/ui_mainwindow.h" line="133"/>
+        <source>F1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/main.cpp" line="10"/>
+        <source>Hello John!</source>
+        <translation>Ahoj Honzo!</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QTranslator>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QTranslator translator;
+    translator.load(":/qm/mtb-config_cz");
+    a.installTranslator(&translator);
+    QObject::tr("Hello John!");
     MainWindow w;
     w.show();
     return a.exec();
