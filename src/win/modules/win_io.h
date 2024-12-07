07a0470973f475f92457d30e07159052074cbb1b
Jan Horacek <me@apophis.cz>
2024-12-07 18:25:22 +0100

    Add IO window template. #5

---
+++ src/win/modules/win_io.h
@@ -0,0 +1,26 @@
+#ifndef WIN_IO_H
+#define WIN_IO_H
+
+#include <QDialog>
+#include <QJsonObject>
+#include "common.h"
+
+class MtbModuleIODialog : public QDialog {
+    Q_OBJECT
+
+public:
+    MtbModuleIODialog(QWidget *parent = nullptr) : QDialog(parent) {}
+
+    virtual void openModule(const QJsonObject& module) = 0;
+    virtual void moduleChanged(const QJsonObject& module) = 0;
+    virtual void inputsChanged(const QJsonObject& module) = 0;
+    virtual void outputsChanged(const QJsonObject& module) = 0;
+    virtual void retranslate() = 0;
+
+protected:
+    uint8_t address;
+    MtbModuleType type;
+
+};
+
+#endif // WIN_MTBUNICONFIG_H
